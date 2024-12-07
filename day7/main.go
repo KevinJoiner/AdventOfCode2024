e2a8362b645955c37d84a4183b9559f77a195464
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-12-07 09:26:40 -0500

    day 7️⃣
    got from 1s execution time to 50ms

--- day7/main.go
+++ day7/main.go
@@ -36,8 +36,8 @@ func main() {
 func puzzle1(rows [][]byte) (any, error) {
 	sum := 0
 	for _, row := range rows {
-		testVal, _, vals := totalsAndNumbers(row)
-		if check(testVal, vals[0], vals[1:], []int{vals[0]}) {
+		testVal, vals := totalsAndNumbers(row)
+		if check(testVal, vals[0], vals[1:]) {
 			sum += testVal
 		}
 	}
@@ -47,15 +47,15 @@ func puzzle1(rows [][]byte) (any, error) {
 func puzzle2(rows [][]byte) (any, error) {
 	sum := 0
 	for _, row := range rows {
-		testVal, _, vals := totalsAndNumbers(row)
-		if check2(testVal, vals[0], vals[1:], []int{vals[0]}) {
+		testVal, vals := totalsAndNumbers(row)
+		if check2(testVal, vals[0], vals[1:]) {
 			sum += testVal
 		}
 	}
 	return sum, nil
 }
 
-func totalsAndNumbers(row []byte) (testVal int, total int, vals []int) {
+func totalsAndNumbers(row []byte) (int, []int) {
 	input := bytes.Split(row, []byte(":"))
 	testVal, err := strconv.Atoi(string(input[0]))
 	if err != nil {
@@ -63,109 +63,59 @@ func totalsAndNumbers(row []byte) (testVal int, total int, vals []int) {
 	}
 	scanner := bufio.NewScanner(bytes.NewReader(input[1]))
 	scanner.Split(bufio.ScanWords)
+	var vals []int
 	for scanner.Scan() {
 		val, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			panic(err)
 		}
 		vals = append(vals, val)
-		total += val
 	}
-	// slices.Reverse(vals)
-	return testVal, total, vals
+	return testVal, vals
 }
 
-// func check(testVal int, addVal int, vals []int) bool {
-// 	if addVal == testVal {
-// 		return true
-// 	}
-// 	if len(vals) == 0 {
-// 		return false
-// 	}
-// 	fmt.Println(vals[0], "+", vals[1:], "=", addVal, "!=", testVal)
-// 	// if all add children are too big break
-// 	if addVal > testVal {
-// 		fmt.Println("break too high add")
-// 		return false
-// 	}
-// 	rawVal := (addVal - vals[0])
-// 	mulNum := rawVal * vals[0]
-
-// 	if mulNum == testVal {
-// 		return true
-// 	}
-// 	fmt.Println(vals[0], "*", vals[1:], "=", mulNum, "!=", testVal)
-
-//		fmt.Println(addVal)
-//		fmt.Println()
-//		if check(testVal, addVal, vals[1:]) {
-//			return true
-//		}
-//		fmt.Println(mulNum)
-//		fmt.Println()
-//		if check(testVal, mulNum, vals[1:]) {
-//			return true
-//		}
-//		return false
-//	}
-func check(testVal int, startNum int, vals []int, seq []int) bool {
+func check(testVal int, startNum int, vals []int) bool {
 	if len(vals) == 0 {
-		return testVal == evalSeq(seq)
+		return testVal == startNum
 	}
 
-	prev := evalSeq(seq)
-	seq = []int{prev, -1, vals[0]}
-	if check(testVal, startNum, vals[1:], seq) {
+	tryVal := startNum + vals[0]
+	if check(testVal, tryVal, vals[1:]) {
 		return true
 	}
 
-	seq = []int{prev, -2, vals[0]}
-	if check(testVal, startNum, vals[1:], seq) {
-		return true
-	}
-	return false
+	tryVal = startNum * vals[0]
+	return check(testVal, tryVal, vals[1:])
 }
 
-func check2(testVal int, startNum int, vals []int, seq []int) bool {
+func check2(testVal int, startNum int, vals []int) bool {
 	if len(vals) == 0 {
-		return testVal == evalSeq(seq)
+		return testVal == startNum
 	}
-	prev := evalSeq(seq)
-	seq = []int{prev, -1, vals[0]}
-	if check2(testVal, startNum, vals[1:], seq) {
+	tryVal := startNum + vals[0]
+	if check2(testVal, tryVal, vals[1:]) {
 		return true
 	}
-	seq = []int{prev, -2, vals[0]}
-	if check2(testVal, startNum, vals[1:], seq) {
+
+	tryVal = startNum * vals[0]
+	if check2(testVal, tryVal, vals[1:]) {
 		return true
 	}
-	seq = []int{prev, -3, vals[0]}
-	if check2(testVal, startNum, vals[1:], seq) {
-		return true
-	}
-	return false
+	tryVal = concat(startNum, vals[0])
+	return check2(testVal, tryVal, vals[1:])
+
 }
 
-func evalSeq(seq []int) int {
-	total := seq[0]
-	for i := 1; i < len(seq); i = i + 2 {
-		val := seq[i+1]
-		if seq[i] == -1 {
-			total += val
-		} else if seq[i] == -2 {
-			total *= val
-		} else {
-			n := 0
-			count := 0
-			for val != 0 {
-				r := val % 10
-				n += r * int(math.Pow10(count))
-				val = val / 10
-				count++
-			}
-			total *= int(math.Pow10(count))
-			total += n
-		}
+func concat(a, b int) int {
+	n := 0
+	count := 0
+	for b != 0 {
+		r := b % 10
+		n += r * int(math.Pow10(count))
+		b = b / 10
+		count++
 	}
-	return total
+	a *= int(math.Pow10(count))
+	a += n
+	return a
 }
